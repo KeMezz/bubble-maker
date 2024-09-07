6fb4e80ecd05f9b22e69ae26fb2041ac97ad32bf
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-09-07 12:12:44 +0900

    install tailwind css

--- app.vue
+++ app.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <div>Hello, World!</div>
+  <div>Count is {{ count }}</div>
+  <button @click="addCount">Add</button>
+  <button @click="subCount">Sub</button>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    addCount() {
+      this.count += 1;
+    },
+    subCount() {
+      this.count -= 1;
+    },
+  },
+};
+</script>
