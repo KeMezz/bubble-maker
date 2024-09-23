1b6dad6de068b616e726dc598413ee7b2b2a1052
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-09-23 22:06:57 +0900

    활성화된 화자 선택하는 버튼 컴포넌트 작성

---
+++ components/select-person-button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button
+    :class="{
+      'px-4 py-2 rounded-lg w-24': true,
+      'bg-purple-400 text-white': isActive,
+      'text-black': !isActive,
+    }"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  name: "SelectPersonButton",
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
