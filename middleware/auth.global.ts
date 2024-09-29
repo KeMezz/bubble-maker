31b33d24136d18f3720004d3015ff34ef3f07ae9
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-09-29 20:57:05 +0900

    세션에서 받아온 userId가 유효한지 검증하는 과정 추가

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,18 +2,38 @@ import { defineNuxtRouteMiddleware } from "nuxt/app";
 import { useSession } from "h3";
 import { useNuxtApp } from "#app";
 import { USE_SESSION_CONFIG } from "~/constants/session-const";
+import prisma from "~/lib/prisma";
+
+async function validateUser(userId: number) {
+  if (!userId) return false;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return !!user;
+}
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const { ssrContext } = useNuxtApp();
 
   if (ssrContext) {
     const event = useRequestEvent();
-    const { data } = await useSession(event!, USE_SESSION_CONFIG);
+    const session = await useSession(event!, USE_SESSION_CONFIG);
 
-    if (!data?.userId && to.path !== "/login") {
+    const isUserValid = await validateUser(+session.data?.userId);
+
+    if (!isUserValid && to.path !== "/login") {
+      await session.clear();
+
       return navigateTo("/login");
     }
-    if (data?.userId && to.path === "/login") {
+    if (isUserValid && to.path === "/login") {
       return navigateTo("/");
     }
   }
