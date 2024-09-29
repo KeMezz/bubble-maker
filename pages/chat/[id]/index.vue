f5e6046cc4ff1c02e953a2ff23141a03136d7af5
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-09-29 20:59:19 +0900

    새로운 메시지 추가하는 기능 작성

--- pages/chat/[id]/index.vue
+++ pages/chat/[id]/index.vue
@@ -10,58 +10,115 @@
   TODO3:  구성원 추가하는 별도 페이지 만들기, 구성원 추가시 프로필사진도 설정할 수 있게 하기
 -->
 <template>
-  <form action="">
-    <div class="bg-sky-200 p-4 h-[80vh] overflow-y-scroll">
-      <div class="flex gap-4">
-        <div class="bg-neutral-300 w-12 h-12 rounded-full" />
-        <!-- TODO: mt-1.5 말고, 프로필 사진 사이즈의 절반으로  -->
-        <div class="flex flex-col gap-3 mt-[6px]">
-          <div
-            class="bg-yellow-200 flex items-center px-4 py-2 rounded-lg w-fit"
-          >
-            반가워요!
-          </div>
-          <div
-            class="bg-yellow-200 flex items-center px-4 py-2 rounded-lg w-fit"
-          >
-            뭔가 말씀을 해주세요.
-          </div>
-        </div>
-      </div>
-      <div class="flex flex-row-reverse gap-4">
-        <div class="flex flex-col items-end gap-3 mt-1.5">
-          <div class="bg-white flex items-center px-4 py-2 rounded-lg w-fit">
-            안녕하세요.
-          </div>
-          <div class="bg-white flex items-center px-4 py-2 rounded-lg w-fit">
-            아래 입력창을 통해 새로운 말풍선을 추가할 수 있군요!
-          </div>
-        </div>
-      </div>
+  <div>
+    <!-- <client-only> -->
+    <div
+      class="bg-sky-200 p-4 min-h-[calc(100vh - 96px)] overflow-y-scroll mb-44 flex flex-col gap-4"
+    >
+      <Message
+        v-for="message in messages"
+        :key="message.id"
+        :text="message.text"
+        :senderType="message.senderType"
+      />
     </div>
-    <div class="flex flex-col">
+    <!-- </client-only> -->
+    <form
+      @submit.prevent="addMessage"
+      class="flex flex-col justify-center items-center gap-6 py-6 fixed bottom-0 left-0 h-42 w-full bg-neutral-100 shadow-xl"
+    >
       <div
-        class="self-center mt-4 px-4 py-2 flex gap-2 bg-neutral-100 w-fit rounded-lg"
+        class="self-center px-3 py-2 flex gap-2 bg-neutral-200 w-fit rounded-lg"
       >
-        <ChatButtonSelectPerson :isActive="true" text="나" />
-        <ChatButtonSelectPerson :isActive="false" text="상대방" />
+        <ChatButtonSelectPerson
+          text="나"
+          :senderId="1"
+          :currentSender="sender"
+          @update:currentSender="(senderId) => setSender(senderId)"
+        />
+        <ChatButtonSelectPerson
+          text="상대방"
+          :senderId="2"
+          :currentSender="sender"
+          @update:currentSender="(senderId) => setSender(senderId)"
+        />
       </div>
-      <div class="flex gap-4 p-4">
+      <div class="flex w-full gap-4 px-4">
         <input
           class="border-2 px-4 py-2 rounded-lg w-full"
           type="text"
+          v-model="text"
           placeholder="메시지를 추가해주세요.."
         />
         <button
+          type="submit"
           class="bg-neutral-500 hover:bg-neutral-400 transition-colors px-4 w-20 rounded-lg text-white"
         >
           송신
         </button>
       </div>
-    </div>
-  </form>
+    </form>
+  </div>
 </template>
 
 <script setup lang="ts">
 // TODO: 최종적으로 스크린샷 생성하는 매커니즘은 어떻게 만들건지? dom-to-image? html-to-image?
+const sender = ref(1);
+const text = ref("");
+
+const route = useRoute();
+const chatId = route.params.id;
+
+const {
+  data: {
+    value: { messages: initialMessages },
+  },
+  status,
+  error,
+  refresh,
+  clear,
+} = await useFetch("/api/messages", {
+  method: "GET",
+  params: { chatId },
+});
+const messages = ref(initialMessages);
+
+function setSender(senderId: number) {
+  if (senderId) {
+    sender.value = senderId;
+  }
+}
+
+async function addMessage() {
+  const { ok, message } = await $fetch<CreateMessageResponse>(`/api/message`, {
+    method: "POST",
+    body: { chatId, sender: sender.value, text: text.value },
+  });
+
+  if (ok) {
+    messages.value = [...messages.value, message];
+  }
+
+  text.value = "";
+}
+
+function scrollToBottom() {
+  nextTick(() => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+    });
+  });
+}
+
+watch(
+  messages,
+  () => {
+    scrollToBottom();
+  },
+  { deep: true }
+);
+
+onMounted(() => {
+  scrollToBottom();
+});
 </script>
